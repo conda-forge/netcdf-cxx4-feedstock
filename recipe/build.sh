881ce53484c8fc126be0a48845185de168b5ae6d
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2016-06-07 15:32:53 +0200

    FIX: Update 4.3.0 and pinning, add patch to generate ncxx4-config

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,38 +1,14 @@
 #!/usr/bin/env bash
 
-if [ "$(uname)" == "Darwin" ]
-then
-    # for Mac OSX
-    export CC=clang
-    export CXX=clang++
-    export MACOSX_VERSION_MIN="10.7"
-    export MACOSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
-    export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
-    export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export LDFLAGS="${LDFLAGS} -stdlib=libc++ -lc++"
-    export LINKFLAGS="${LDFLAGS}"
-    # See http://www.unidata.ucar.edu/support/help/MailArchives/netcdf/msg11939.html
-    export DYLD_LIBRARY_PATH=${PREFIX}/lib
-elif [ "$(uname)" == "Linux" ]
-then
-    # for Linux
-    export CC=gcc
-    export CXX=g++
-    export CXXFLAGS="${CXXFLAGS} -DBOOST_MATH_DISABLE_FLOAT128"
-    export LDFLAGS="${LDFLAGS}"
-    export LINKFLAGS="${LDFLAGS}"
-else
-    echo "This system is unsupported by the toolchain."
-    exit 1
-fi
-
-export CFLAGS="${CFLAGS} -m${ARCH}"
-export CXXFLAGS="${CXXFLAGS} -m${ARCH}"
-
-
-CPPFLAGS=-I$PREFIX/include LDFLAGS=-L$PREFIX/lib ./configure --prefix=$PREFIX
-
+# Build shared library using cmake
+# works for both linux and osx, but missing ncxx4-config
+# see https://github.com/Unidata/netcdf-cxx4/issues/36
+cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
+      -D CMAKE_INSTALL_LIBDIR:PATH=$PREFIX/lib \
+      -D BUILD_SHARED_LIBS=ON \
+      -D NCXX_ENABLE_TESTS=ON \
+      -D ENABLE_DOXYGEN=OFF \
+      $SRC_DIR
 make
-make check
+ctest
 make install
