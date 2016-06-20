f6c53bf136397a44c104d9b23094672a1111c025
ocefpaf <ocefpaf@gmail.com>
2016-06-20 11:08:00 -0300

    drop ncxx4-config, test libs, build static closes #9

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,26 @@
 # Build shared library using cmake
 # works for both linux and osx, but missing ncxx4-config
 # see https://github.com/Unidata/netcdf-cxx4/issues/36
+
+# Build static.
+mkdir build_static && cd build_static
+cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
+      -D CMAKE_INSTALL_LIBDIR:PATH=$PREFIX/lib \
+      -D BUILD_SHARED_LIBS=OFF \
+      -D NCXX_ENABLE_TESTS=ON \
+      -D ENABLE_DOXYGEN=OFF \
+      $SRC_DIR
+make
+# ctest  # Run only for the shared lib build to save time.
+make install
+
+
+make clean
+
+cd ..
+
+# Build shared.
+mkdir build_shared && cd build_shared
 cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D CMAKE_INSTALL_LIBDIR:PATH=$PREFIX/lib \
       -D BUILD_SHARED_LIBS=ON \
---
+++ recipe/run_test.py
@@ -0,0 +1,18 @@
+# Load the libraries using ctypes.
+import os
+import sys
+import ctypes
+
+platform = sys.platform
+
+if platform.startswith('linux'):
+    path = os.path.join(sys.prefix, 'lib', 'libnetcdf-cxx4.so')
+    lib = ctypes.CDLL(path)
+elif platform == 'darwin':
+    path = os.path.join(sys.prefix, 'lib', 'libnetcdf-cxx4.dylib')
+    lib = ctypes.CDLL(path)
+elif platform == 'win32':
+    path = os.path.join(sys.prefix, 'Library', 'bin', 'libnetcdf-cxx4.dll')
+    lib = ctypes.CDLL(path)
+else:
+    raise ValueError('Unrecognized platform: {}'.format(platform))
