5aad88abe7898b2e4c9e485a384833ce176fccba
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-07-17 10:15:54 +0300

    first changes to allow win

---
+++ recipe/bld.bat
@@ -0,0 +1,29 @@
+@echo on
+
+setlocal EnableDelayedExpansion
+
+if "%mpi%" NEQ "" if "%mpi%" NEQ "nompi" (
+  set "CC=mpicc"
+  set "CXX=mpicxx"
+)
+
+set "BUILD_DIR=%SRC_DIR%\build_shared"
+mkdir "%BUILD_DIR%"
+cd "%BUILD_DIR%"
+
+cmake %CMAKE_ARGS% ^
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_C_COMPILER=%CC% ^
+    -DCMAKE_CXX_COMPILER=%CXX% ^
+    -DBUILD_SHARED_LIBS=ON ^
+    -DNCXX_ENABLE_TESTS=OFF ^
+    -DENABLE_DOXYGEN=OFF ^
+    -DHDF5_ROOT=%LIBRARY_PREFIX% ^
+    "%SRC_DIR%"
+
+cmake --build . --config Release --target install
+
+REM Create symbolic link if necessary (Windows equivalent, copying instead of linking)
+copy "%LIBRARY_PREFIX%\lib\netcdf-cxx4.lib" "%LIBRARY_PREFIX%\lib\netcdf_c++4.lib"
